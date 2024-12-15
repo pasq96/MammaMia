864077981956086d9006f7bbdd83339fb6bba2f9
Pasquale Coppola <pasq96@users.noreply.github.com>
2024-12-15 04:31:05 +0100

    Update tantifilm.py

--- Src/API/tantifilm.py
+++ Src/API/tantifilm.py
@@ -13,58 +13,67 @@ TF_DOMAIN = config.TF_DOMAIN
 import urllib.parse
 
 async def search(showname, ismovie, date, client):
-    showname = urllib.parse.quote_plus(showname)
+    showname = urllib.parse.quote_plus(showname.replace(" ", "%20"))
     url = f'https://www.tanti.bond/ajax/posts?q={showname}'
     response = await client.post(url, allow_redirects=True, impersonate="chrome120")
     response = response.json()['data']
     
-    for link in response:
-        base_url = link['url']
-        response = await client.get(base_url, allow_redirects=True, impersonate="chrome120")
-        soup = BeautifulSoup(response.text, 'lxml')
-        release_date = soup.find('div', class_='release-date').get_text(strip=True)
-        
-        if release_date == date:
-            tid = base_url.split('-')[-1]
-            if ismovie:
-                return tid, base_url
-            else:
-                season_url = f'{base_url}-1-season-1-episode'
-                response = await client.get(season_url, allow_redirects=True, impersonate="chrome120")
-                soup = BeautifulSoup(response.text, 'lxml')
-                embed_id = soup.find('a', class_='dropdown-toggle btn-service selected')['data-embed']
-                return season_url, embed_id
-    return None, None
+    if ismovie == 1:
+        for link in response:
+            url = link['url']
+            response = await client.get(url, allow_redirects=True, impersonate="chrome120")
+            pattern = r'Data di rilascio\s*</div>\s*<div class="text">\s*(\d{4})\s*</div>'
+            found_date = re.search(pattern, await response.text())
+            release_date = str(found_date.group(1))
+            if release_date == date:
+                tid = url.split('-')[-1]
+                return tid, url
+    elif ismovie == 0: 
+        for link in response:
+            base_url = link['url']
+            url = f'{base_url}-1-season-1-episode'
+            response = await client.get(url, allow_redirects=True, impersonate="chrome120")
+            pattern = r'Data di rilascio\s*</div>\s*<div class="text">\s*(\d{4})\s*</div>'
+            found_date = re.search(pattern, await response.text())
+            release_date = str(found_date.group(1))
+            if release_date == date:
+                tid = url.split('-')[1]
+                soup = BeautifulSoup(await response.text(), 'lxml')
+                a_tag = soup.find('a', class_='dropdown-toggle btn-service selected')
+                embed_id = a_tag['data-embed']
+                return url, embed_id
 
 async def fast_search(showname, ismovie, client):
-    showname = urllib.parse.quote_plus(showname)
+    showname = urllib.parse.quote_plus(showname.replace(" ", "%20"))
     url = f'https://www.tanti.{TF_DOMAIN}/search/{showname}'
     response = await client.get(url, allow_redirects=True, impersonate="chrome120")
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    if ismovie:
+    soup = BeautifulSoup(await response.text(), "lxml")
+    
+    if ismovie == 1:
         first_link = soup.select_one('#movies .col .list-media')
-        tid = first_link['href'].split('-')[1]
-        return tid, first_link['href']
-    else:
+        url = first_link['href']
+        tid = url.split('-')[1]
+        return tid, url
+    elif ismovie == 0: 
         first_link = soup.select_one('#series .col .list-media')
         base_url = first_link['href']
-        season_url = f'{base_url}-1-season-1-episode'
-        response = await client.get(season_url, allow_redirects=True, impersonate="chrome120")
-        soup = BeautifulSoup(response.text, 'lxml')
-        embed_id = soup.find('a', class_='dropdown-toggle btn-service selected')['data-embed']
-        return season_url, embed_id
+        url = f'{base_url}-1-season-1-episode'
+        response = await client.get(url, allow_redirects=True, impersonate="chrome120")
+        soup = BeautifulSoup(await response.text(), 'lxml')
+        a_tag = soup.find('a', class_='dropdown-toggle btn-service selected')
+        embed_id = a_tag['data-embed']
+        return url, embed_id
 
 async def get_protect_link(id, url, client):
     response = await client.get(f"https://p.hdplayer.casa/myadmin/play.php?id={id}", allow_redirects=True, impersonate="chrome120")
-    soup = BeautifulSoup(response.text, "lxml", parse_only=SoupStrainer('iframe'))
+    soup = BeautifulSoup(await response.text(), "lxml", parse_only=SoupStrainer('iframe'))
     protect_link = soup.iframe['src']
     
     if "protect" in protect_link:
         return protect_link
     else:
         response = await client.get(url, allow_redirects=True, impersonate="chrome120")
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(await response.text(), 'lxml')
         embed_id = soup.find('a', class_='dropdown-toggle btn-service selected')['data-embed']
         headers = {'User-Agent': 'Mozilla/5.0', 'Referer': url}
         data = {'id': embed_id}
@@ -72,7 +81,7 @@ async def get_protect_link(id, url, client):
         response = await client.post(ajax_url, headers=headers, data=data)
         hdplayer = response.text[43:-27]
         response = await client.get(hdplayer, allow_redirects=True, impersonate="chrome120")
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(await response.text(), 'lxml')
         
         links_dict = {}
         li_tags = soup.select('ul.nav.navbar-nav li.dropdown')
@@ -83,7 +92,7 @@ async def get_protect_link(id, url, client):
                 if title != "1" and "Tantifilm" not in title:
                     href = a_tag['href']
                     response = await client.get(href, allow_redirects=True, impersonate="chrome120")
-                    soup = BeautifulSoup(response.text, "lxml", parse_only=SoupStrainer('iframe'))
+                    soup = BeautifulSoup(await response.text(), "lxml", parse_only=SoupStrainer('iframe'))
                     protect_link = soup.iframe['src']
                     if "protect" in protect_link:
                         links_dict[title] = protect_link
@@ -110,12 +119,12 @@ async def true_url(protect_link, client):
     if response.status_code == 200:
         real_time = str(int(time.time()))
         pattern = r"(\/pass_md5\/.*?)'.*(\?token=.*?expiry=)"
-        match = re.search(pattern, response.text, re.DOTALL)
+        match = re.search(pattern, await response.text(), re.DOTALL)
         
         if match:
             url = f'https://d000d.com{match[1]}'
             rebobo = await client.get(ForwardProxy + url, headers=headers, allow_redirects=True, impersonate="chrome120", proxies=proxies)
-            real_url = f'{rebobo.text}123456789{match[2]}{real_time}'
+            real_url = f'{await rebobo.text()}123456789{match[2]}{real_time}'
             print("MammaMia: Found results for Tantifilm")
             return real_url
         else:
@@ -125,53 +134,67 @@ async def true_url(protect_link, client):
     return None
 
 async def tantifilm(imdb, client, TF_FAST_SEARCH):
+    urls = None
     try:
         general = is_movie(imdb)
         ismovie = general[0]
         imdb_id = general[1]
-        season, episode = int(general[2]), int(general[3]) if ismovie == 0 else (None, None)
-        
-        if ismovie == 0:
+        if ismovie == 0 : 
+            season = int(general[2])
+            episode = int(general[3])
             if "tt" in imdb:
                 if TF_FAST_SEARCH == "0":
-                    showname, date = await get_info_imdb(imdb_id, ismovie, "Tantifilm", client)
+                    type = "Tantifilm"
+                    showname, date = await get_info_imdb(imdb_id, ismovie, type, client)
                     url, embed_id = await search(showname, ismovie, date, client)
                 elif TF_FAST_SEARCH == "1":
-                    showname = await get_info_imdb(imdb_id, ismovie, "TantifilmFS", client)
+                    type = "TantifilmFS"
+                    showname = await get_info_imdb(imdb_id, ismovie, type, client)
                     url, embed_id = await fast_search(showname, ismovie, client)
             else:
                 tmdba = imdb_id.replace("tmdb:", "")
                 if TF_FAST_SEARCH == "0":
-                    showname, date = get_info_tmdb(tmdba, ismovie, "Tantifilm")
+                    type = "Tantifilm"
+                    showname, date = get_info_tmdb(tmdba, ismovie, type)
                     url, embed_id = await search(showname, ismovie, date, client)
                 elif TF_FAST_SEARCH == "1":
-                    showname = get_info_tmdb(tmdba, ismovie, "TantifilmFS")
+                    type = "TantifilmFS"
+                    showname = get_info_tmdb(tmdba, ismovie, type)
                     url, embed_id = await fast_search(showname, ismovie, client)
-            protect_link = await get_protect_link(url, embed_id, client)
-            true_url_link = await true_url(protect_link, client)
-            return true_url_link
-        
+            protect_link = await get_nuovo_indirizzo_and_protect_link(url, embed_id, season, episode, client)
+            url = await true_url(protect_link, client)
+            return url
         elif ismovie == 1:
             if "tt" in imdb:
                 if TF_FAST_SEARCH == "0":
-                    showname, date = await get_info_imdb(imdb_id, ismovie, "Tantifilm", client)
+                    type = "Tantifilm"
+                    showname, date = await get_info_imdb(imdb_id, ismovie, type, client)
                     tid, url = await search(showname, ismovie, date, client)
                 elif TF_FAST_SEARCH == "1":
-                    showname = await get_info_imdb(imdb_id, ismovie, "TantifilmFS", client)
+                    type = "TantifilmFS"
+                    showname = await get_info_imdb(imdb_id, ismovie, type, client)
+                    date = None
                     tid, url = await fast_search(showname, ismovie, client)
             else:
                 if TF_FAST_SEARCH == "0":
-                    showname, date = get_info_tmdb(imdb, ismovie, "Tantifilm")
+                    type = "Tantifilm"
+                    showname, date = get_info_tmdb(imdb, ismovie, type)
                     tid, url = await search(showname, ismovie, date, client)
                 elif TF_FAST_SEARCH == "1":
-                    showname = get_info_tmdb(imdb, ismovie, "TantifilmFS")
+                    type = "TantifilmFS"
+                    showname = get_info_tmdb(imdb, ismovie, type)
                     tid, url = await fast_search(showname, ismovie, client)
             protect_link = await get_protect_link(tid, url, client)
-            if isinstance(protect_link, str):
-                true_url_link = await true_url(protect_link, client)
-                return true_url_link
-            return protect_link
-
+            if not isinstance(protect_link, str):
+                urls = protect_link
+                if urls:
+                    return urls
+                else:
+                    print("Tantifilm Error v2")
+            else:
+                url = await true_url(protect_link, client)
+                if url:
+                    return url
     except Exception as e:
         print("Tantifilm Error: ", e)
         return None
